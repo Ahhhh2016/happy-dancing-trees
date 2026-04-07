8bdce1a306b66dd0070fbf886bd78e61701fed68
Jake (Maggie) Ronan <maggie_ronan@brown.edu>
2026-04-07 18:41:17 -0400

    update monster.h

--- monster.h
+++ monster.h
@@ -1,10 +1,21 @@
 #ifndef MONSTER_H
 #define MONSTER_H
 
-class monster
-{
+#include <Eigen/Dense>
+#include <vector>
+
+struct Stroke {
+    std::vector<Eigen::Vector2d> D;
+    bool isClosed;
+    int depthOrder;
+};
+
+class monster {
 public:
     monster();
+    Stroke makeDummyBody();
+    Stroke makeDummyLeg();
 };
+
 
 #endif // MONSTER_H
